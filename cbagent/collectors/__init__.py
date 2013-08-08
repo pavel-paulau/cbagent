cbb3192e0345b19c6ea3d54478edf9a5826b8de4
pavel-paulau <pavel.paulau@gmail.com>
2013-08-08 17:24:33 +0300

    add short shortcut for import
    Change-Id: Ifdedebc8383acb5ed93abc80ffadd2cc8ecae32d

--- cbagent/collectors/__init__.py
+++ cbagent/collectors/__init__.py
@@ -3,4 +3,5 @@ from ns_server import NSServer
 from atop import Atop
 from active_tasks import ActiveTasks
 from latency import Latency
+from spring_latency import SpringLatency, SpringQueryLatency
 from xdcr_lag import XdcrLag
