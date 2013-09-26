c22835900fd874626c014e0e3df016c9c1b464a2
pavel-paulau <pavel.paulau@gmail.com>
2013-09-26 11:21:52 -0700

    leverage decorator package
    Change-Id: I95336a336c9ac923de30643f1d33c814d1a1dfb8

--- cbagent/collectors/libstats/remotestats.py
+++ cbagent/collectors/libstats/remotestats.py
@@ -1,24 +1,24 @@
+from decorator import decorator
+
 from fabric.api import hide, settings, parallel
 from fabric.tasks import execute
 
 
-def multi_node_task(task):
-    def wrapper(*args, **kargs):
-        self = args[0]
-        with settings(user=self.user, password=self.password, warn_only=True):
-            with hide("output"):
-                return execute(parallel(task), *args, hosts=self.hosts, **kargs)
-    return wrapper
-
-
-def single_node_task(task):
-    def wrapper(*args, **kargs):
-        self = args[0]
+@decorator
+def multi_node_task(task, *args, **kargs):
+    self = args[0]
+    with settings(user=self.user, password=self.password, warn_only=True):
         with hide("output"):
-            with settings(host_string=self.hosts[0], user=self.user,
-                          password=self.password, warn_only=True):
-                return task(*args, **kargs)
-    return wrapper
+            return execute(parallel(task), *args, hosts=self.hosts, **kargs)
+
+
+@decorator
+def single_node_task(task, *args, **kargs):
+    self = args[0]
+    with settings(user=self.user, password=self.password, warn_only=True,
+                  host_string=self.hosts[0]):
+        with hide("output"):
+            return task(*args, **kargs)
 
 
 class RemoteStats(object):
