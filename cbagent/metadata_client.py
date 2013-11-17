0f37f0d819f2b7249ece00b137fd7e8ce5b3dff8
pavel-paulau <pavel.paulau@gmail.com>
2013-11-16 22:21:16 -0800

    don't add clusters/servers/buckets if they already exist
    Change-Id: I3f56025c668a126e579b22c0457f11e8924ad34c

--- cbagent/metadata_client.py
+++ cbagent/metadata_client.py
@@ -23,14 +23,14 @@ def interrupt(request, *args, **kargs):
 class RestClient(object):
 
     @interrupt
-    def post(self, url, params):
-        r = requests.post(url, params)
+    def post(self, url, data):
+        r = requests.post(url=url, data=data)
         if r.status_code == 500:
             raise InternalServerError(url)
 
     @interrupt
-    def get(self, url):
-        r = requests.get(url)
+    def get(self, url, params):
+        r = requests.get(url=url, params=params)
         if r.status_code == 500:
             raise InternalServerError(url)
         return r.json()
@@ -43,50 +43,73 @@ class MetadataClient(RestClient):
         self.base_url = "http://{0}/cbmonitor".format(
             settings.cbmonitor_host_port)
 
+    def get_clusters(self):
+        url = self.base_url + "/get_clusters/"
+        return self.get(url, {})
+
+    def get_servers(self):
+        url = self.base_url + "/get_servers/"
+        params = {"cluster": self.settings.cluster}
+        return self.get(url, params)
+
+    def get_buckets(self):
+        url = self.base_url + "/get_buckets/"
+        params = {"cluster": self.settings.cluster}
+        return self.get(url, params)
+
     def add_cluster(self):
-        logger.info("Adding cluster: {0}".format(self.settings.cluster))
+        if self.settings.cluster in self.get_clusters():
+            return
 
         url = self.base_url + "/add_cluster/"
-        params = {"name": self.settings.cluster,
-                  "rest_username": self.settings.rest_username,
-                  "rest_password": self.settings.rest_password}
-        self.post(url, params)
+        data = {"name": self.settings.cluster,
+                "rest_username": self.settings.rest_username,
+                "rest_password": self.settings.rest_password}
+
+        logger.info("Adding cluster: {0}".format(self.settings.cluster))
+        self.post(url, data)
 
     def add_server(self, address):
-        logger.info("Adding server: {0}".format(address))
+        if address in self.get_servers():
+            return
 
         url = self.base_url + "/add_server/"
-        params = {"address": address,
-                  "cluster": self.settings.cluster,
-                  "ssh_username": self.settings.ssh_username,
-                  "ssh_password": self.settings.ssh_password}
-        self.post(url, params)
+        data = {"address": address,
+                "cluster": self.settings.cluster,
+                "ssh_username": self.settings.ssh_username,
+                "ssh_password": self.settings.ssh_password}
+
+        logger.info("Adding server: {0}".format(address))
+        self.post(url, data)
 
     def add_bucket(self, name):
+        if name in self.get_buckets():
+            return
+
         logger.info("Adding bucket: {0}".format(name))
 
         url = self.base_url + "/add_bucket/"
-        params = {"name": name, "type": "Couchbase",
-                  "cluster": self.settings.cluster}
-        self.post(url, params)
+        data = {"name": name, "type": "Couchbase",
+                "cluster": self.settings.cluster}
+        self.post(url, data)
 
     def add_metric(self, name, bucket=None, server=None, unit=None,
                    description=None, collector=None):
         logger.debug("Adding metric: {0}".format(name))
 
         url = self.base_url + "/add_metric_or_event/"
-        params = {"name": name, "type": "metric",
-                  "cluster": self.settings.cluster}
+        data = {"name": name, "type": "metric",
+                "cluster": self.settings.cluster}
         for extra_param in ("bucket", "server", "unit", "description",
                             "collector"):
             if eval(extra_param) is not None:
-                params[extra_param] = eval(extra_param)
-        self.post(url, params)
+                data[extra_param] = eval(extra_param)
+        self.post(url, data)
 
     def add_snapshot(self, name, ts_from, ts_to):
         logger.info("Adding snapshot: {0}".format(name))
 
         url = self.base_url + "/add_snapshot/"
-        params = {"cluster": self.settings.cluster, "name": name,
-                  "ts_from": ts_from, "ts_to": ts_to}
-        self.post(url, params)
+        data = {"cluster": self.settings.cluster, "name": name,
+                "ts_from": ts_from, "ts_to": ts_to}
+        self.post(url, data)
