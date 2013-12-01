ba3ff6c8bd4a234d0ce6c082c926bb54dabb02b5
pavel-paulau <pavel.paulau@gmail.com>
2013-11-30 19:56:41 -0800

    simplify active tasts collection
    collect only cluster wide rebalance and compaction progress
    
    Change-Id: I139d80628c9eb72d5f93b5d347a49ffb50b5d02b

--- cbagent/collectors/active_tasks.py
+++ cbagent/collectors/active_tasks.py
@@ -7,58 +7,23 @@ class ActiveTasks(Collector):
 
     def update_metadata(self):
         self.mc.add_cluster()
-
         for bucket in self.get_buckets():
             self.mc.add_bucket(bucket)
 
-        for node in self.get_nodes():
-            self.mc.add_server(node)
-
-    @staticmethod
-    def _build_couchdb_task_id(task, metric):
-        return "{0}_{1}_{2}".format(task["type"], task.get("indexer_type", ""),
-                                    metric)
-
-    def _get_couchdb_tasks(self, server):
-        tasks = self.get_http(path="/_active_tasks", server=server, port=8092)
-        for task in tasks:
-            if "index_barrier" in task["type"]:
-                yield "running_" + task["type"], task["running"], None, None
-                yield "waiting_" + task["type"], task["waiting"], None, None
-            elif task["type"] in ("view_compaction", "indexer"):
-                for metric in ("changes_done", "total_changes", "progress"):
-                    value = task.get(metric, None)
-                    if value is not None:
-                        metric = self._build_couchdb_task_id(task, metric)
-                        bucket = task.get("set", "")
-                        yield metric, value, bucket, server
-
-    @staticmethod
-    def _build_ns_server_task_id(task, metric):
-        task_id = "{0}{1}_{2}".format(task["type"],
-                                      task.get("designDocument", ""),
-                                      metric)
-        return task_id.replace('/', '_')
-
-    def _get_ns_server_tasks(self):
-        tasks = self.get_http(path="/pools/default/tasks")
-        for task in tasks:
-            bucket = task.get("bucket", None)
-            for metric in ("changesDone", "totalChanges", "progress"):
-                value = task.get(metric, 0)
-                metric = self._build_ns_server_task_id(task, metric)
-                yield metric, value, bucket
-
-    def _append(self, metric, value, bucket=None, server=None):
-        self._update_metric_metadata(metric, bucket, server)
-
-        data = {metric: value}
-        self.store.append(data, cluster=self.cluster, bucket=bucket,
-                          server=server, collector=self.COLLECTOR)
+    def _get_tasks(self):
+        tasks = {
+            "bucket_compaction_progress": (0, None),
+            "rebalance_progress": (0, None)
+        }
+        for task in self.get_http(path="/pools/default/tasks"):
+            _task = "{0}_progress".format(task["type"])
+            tasks[_task] = (task.get("progress", 0), task.get("bucket", None))
+        for task, (progress, bucket) in tasks.items():
+            yield task, progress, bucket
 
     def sample(self):
-        for metric, value, bucket in self._get_ns_server_tasks():
-            self._append(metric, value, bucket=bucket)
-        for node in self.get_nodes():
-            for metric, value, bucket, server in self._get_couchdb_tasks(node):
-                self._append(metric, value, bucket, server)
+        for task, progress, bucket in self._get_tasks():
+            self._update_metric_metadata(metric=task, bucket=bucket)
+            self.store.append(data={task: progress},
+                              cluster=self.cluster, bucket=bucket,
+                              collector=self.COLLECTOR)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '1.10.0'
+version = '1.11.0'
 
 setup(
     name='cbagent',
